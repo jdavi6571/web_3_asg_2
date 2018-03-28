c758e13a5ae141ba43c3a3dbce284f76f24a6641
Khaled Kabalan <kkaba678@mtroyal.ca>
2018-03-28 04:24:04 +0000

    test branch3

--- companies.js
+++ companies.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+// Use express to listen to port
+var port = process.env.PORT || 8080;
+
 app.route('/test')
     .get(function(req,res){
         res.send("Test Worked");
     });
 
-// Use express to listen to port
-var port = process.env.PORT || 8080;
+
 
 app.listen(port, function () {
  console.log("Server running at port = " + port);
