73689cbd8d4106f3a9750e88effc52e3e63f7969
Khaled Kabalan <kkaba678@mtroyal.ca>
2018-04-09 05:20:59 +0000

    added cross-origin-resource sharing to server.js

--- server.js
+++ server.js
@@ -63,6 +63,25 @@ var app = express();
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
+//code pulled from: https://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080', 'https://web3asg2.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.route('/companies/stocks/:symbol')
     .get(function (req, resp) {
